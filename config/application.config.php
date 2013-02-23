b0f2f852bdc4ea0b51b1ef0ffec0756a907fd059
Geoffrey de Kleijn <jay@jayhaabee.nl>
2013-02-23 07:44:17 -0500

    Added phpinfo page, route and link in layout

--- config/application.config.php
+++ config/application.config.php
@@ -2,7 +2,7 @@
 return array(
     'modules' => array(
         'Application',
-				'Hypem'
+		'Hypem'
     ),
     'module_listener_options' => array(
         'config_glob_paths'    => array(
--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -20,6 +20,17 @@ return array(
                     ),
                 ),
             ),
+			'info' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/info',
+                    'defaults' => array(
+                        'controller' => 'Application\Controller\Index',
+                        'action'     => 'info',
+                    ),
+                ),
+			),
+
             // The following is a route to simplify getting started creating
             // new controllers and actions without needing to create a new
             // module. Simply drop new controllers in, and you can access them
